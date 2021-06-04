6046fbe4154ba3c7185612b0fdabc82cfafc8b21
takashifuruya0 <takashi.furuya.0@gmail.com>
2021-06-04 11:24:29 +0900

    修正

--- fk_management/settings.py
+++ fk_management/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h)kw2#3knd9ft)2q(9w@n(imv(gox4$hz&la^)1qntq@)!ze01'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
