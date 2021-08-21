8ecd02122597fdf7b72925bec6dad425e8e8fd2e
Takashi Furuya <takashi.furuya.0@gmail.com>
2021-08-21 20:29:59 +0900

    - add library info to README - modify password_rest.html and docker-compose.yaml - add production settings

---
+++ fk_management/environment/production.py
@@ -0,0 +1,24 @@
+from fk_management.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': env('DB_NAME'),
+        "USER": env('DB_USER'),
+        "PASSWORD": env('DB_PASSWORD'),
+        'HOST': env("DB_HOST"),
+        "PORT": env('DB_PORT'),
+        "ATOMIC_REQUESTS": True,
+    }
+}
+
+ENVIRONMENT = "production"
+DEBUG = False
+
+# sendgrid by django-sendgrid-v5==1.1.0
+EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
+SENDGRID_API_KEY = env("SENDGRID_API_KEY", None)
+SENDGRID_SANDBOX_MODE_IN_DEBUG = False
+DEFAULT_FROM_EMAIL = "noreply@fk-management.com"
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+ACCOUNT_EMAIL_REQUIRED = "true"
