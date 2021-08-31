b1063698d7274611b10430bf5b086b679470619f
Takashi Furuya <takashi.furuya.0@gmail.com>
2021-08-31 23:17:50 +0900

    add tests on ReasonWinLoss and Entry

--- asset/tests/test_model_order.py
+++ asset/tests/test_model_order.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.db.utils import IntegrityError
 from django.db import transaction
 from django.contrib.auth import get_user_model
-from asset.models import Stock, Order, Entry, ReasonWinLoss, Ipo, Dividend
+from asset.models import Stock, Order, Entry, ReasonWinLoss, Ipo, Dividend, StockValueData
 from datetime import datetime, timezone
 
 
@@ -63,14 +63,102 @@ class ReasonWinLossTest(TestCase):
     """
     !Test for ReasonWinLoss
     """
-    pass
+    @classmethod
+    def setUpTestData(cls):
+        get_user_model().objects.all().delete()
+        get_user_model().objects.create(
+            username="takashi", password="test", is_superuser=True, is_staff=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        get_user_model().objects.all().delete()
+
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.first()
+        self.client.login()
+
+    def tearDown(self) -> None:
+        self.client.logout()
+        return super().tearDown()
+
+    def test_reason_win_loss(self):
+        """
+        ReasonWinLoss, Normal
+        """
+        # prepare
+        d1 = {
+            "is_win": True, "name": "Won", "memo": "Won because I'm the only appliciant."
+        }
+        r = ReasonWinLoss.objects.create(**d1)
+        # test
+        test_scenarios = [
+            (ReasonWinLoss.objects.all().count(), 1, "count"),
+            (str(r), d1['name'], "__str__")
+        ]
+        for result, expectation, name in test_scenarios:
+            with self.subTest(result=result, expectation=expectation, name=name):
+                self.assertEqual(result, expectation)
 
 
 class EntryTest(TestCase):
     """
     !Test for Entry
     """
-    pass
+    """
+    !Test for Order
+    """
+    @classmethod
+    def setUpTestData(cls):
+        get_user_model().objects.all().delete()
+        get_user_model().objects.create(
+            username="takashi", password="test", is_superuser=True, is_staff=True)
+        Stock.objects.create(code="0000", name="ABC Company")
+        Stock.objects.create(code="0001", name="DEF Company")
+        Stock.objects.create(code="ABC0000EFG", name="newe trust", is_trust=True)
+
+    @classmethod
+    def tearDownClass(cls):
+        super().tearDownClass()
+        get_user_model().objects.all().delete()
+
+    def setUp(self) -> None:
+        self.user = get_user_model().objects.first()
+        self.client.login()
+        self.s0 = Stock.objects.get(code="0000")
+        self.s1 = Stock.objects.get(code="0001")
+        self.st = Stock.objects.get(is_trust=True)
+
+    def tearDown(self) -> None:
+        self.client.logout()
+        return super().tearDown()
+
+    def test_entry(self):
+        """
+        Entry, Normal
+        """
+        # prepare
+        e1 = Entry.objects.create(stock=self.s0)
+        e2 =Entry.objects.create(stock=self.s1)
+        d2 = {
+            "stock": self.s1,
+            "datetime": datetime.now(timezone.utc),
+            "is_buy": True,
+            "val": 1000.1,
+            "num": 100,
+            "commission": 525,
+            "entry": e2
+        }
+        o2 = Order.objects.create(**d2)
+        # test
+        test_scenarios = [
+            (Entry.objects.all().count(), 2, "count"),
+            (str(e1), f"P{e1.pk:0>3}_{e1.stock}", "__str__"),
+            (str(e2), f"O{e2.pk:0>3}_{e2.stock}", "__str__")
+        ]
+        for result, expectation, name in test_scenarios:
+            with self.subTest(result=result, expectation=expectation, name=name):
+                self.assertEqual(result, expectation)
 
 
 class IpoTest(TestCase):
