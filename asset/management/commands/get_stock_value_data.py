b6b06da590ef012d27c841461acc06209c86c3ca
Takashi Furuya <takashi.furuya.0@gmail.com>
2021-09-14 07:25:09 +0900

    FM-123 spped up the command by adding date_range_from param

--- asset/management/commands/get_stock_value_data.py
+++ asset/management/commands/get_stock_value_data.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
+from django.db.models import Max
 from asset.models import Stock, StockValueData
 import requests
 import pprint
@@ -14,9 +15,17 @@ class Command(BaseCommand):
         
     # コマンドが実行された際に呼ばれるメソッド
     def handle(self, *args, **options):
-        url = "https://www.fk-management.com/drm/web/stockvaluedata/?limit=1000"
+        # check the latest date of SVD
+        if StockValueData.objects.exists():
+            ldate = StockValueData.objects.aggregate(ldate=Max('date'))['ldate']
+            url = f"https://www.fk-management.com/drm/web/stockvaluedata/?limit=1000&date_range_after={ldate}"
+        else:
+            url = "https://www.fk-management.com/drm/web/stockvaluedata/?limit=1000"
+        self.stdout.write(f"url: {url}")
+        # init list
         stock_list = list()
         error_list = list()
+        # start loop
         while True:
             r = requests.get(url)
             if not r.status_code == 200:
